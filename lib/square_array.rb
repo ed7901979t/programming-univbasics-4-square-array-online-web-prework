62c1ea9db94dd8c6d7100c1a87282b40fdec1d7b
clean-command-1851 <ed7901979t@gmail.com>
2019-07-29 16:26:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
  numbers = [1,2,3]
  counter =0    
- while numbers[] < numbers.length(3) do 
+ while numbers[1,2,3] do 
    items=numbers.Math.sqrt()
    puts "The results are #{items}"
    counter += 1 
